13d692bb7fe292b8e1a55ab91c6d8684b298fe34
Sam Kidman <samk@localhost.localdomain>
2018-12-06 20:41:42 +1100

    setup hostgroups interface

--- src/app/nagios-objects/reducers/hostgroups.reducer.ts
+++ src/app/nagios-objects/reducers/hostgroups.reducer.ts
@@ -2,11 +2,11 @@ import { Action } from '@ngrx/store';
 
 
 export interface State {
-
+  
 }
 
 export const initialState: State = {
-
+  
 };
 
 export function reducer(state = initialState, action: Action): State {
